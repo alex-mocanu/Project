1be76c99b5f31c818f4af8ebb68181f93a823f2a
Alexandru Mocanu <alexmoc1105@gmail.com>
2018-11-25 18:30:31 +0100

    Further analysis on the Stack Overflow dataset and change of the wikiDownload script

--- wikiDownload.py
+++ wikiDownload.py
@@ -3,6 +3,7 @@ import contextlib
 from datetime import datetime
 import time
 
+
 def call(url):
     req = urllib.request.Request(url)
     with contextlib.closing(urllib.request.urlopen(req)) as response:
@@ -11,36 +12,34 @@ def call(url):
         data = json.loads(request)
     return data
 
-timeStop = "2018-11-17 00:00:00"
-timeStart = "2018-10-18 00:00:00"
-tStop = datetime.strptime(timeStop, "%Y-%m-%d %H:%M:%S")
-tIt = datetime.strptime(timeStart, "%Y-%m-%d %H:%M:%S")
-timestamp = int(tIt.timestamp())
-stopToken = True
 
+if __name__ == '__main__':
+	timeStop = "2018-11-12 01:00:00"
+	timeStart = "2018-10-29 01:00:00"
+	tStop = datetime.strptime(timeStop, "%Y-%m-%d %H:%M:%S")
+	tIt = datetime.strptime(timeStart, "%Y-%m-%d %H:%M:%S")
+	timestamp = int(tIt.timestamp())
+	stopToken = True
+	wikistamp = datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%dT%H:%M:%SZ')
 
-wikistamp = datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%dT%H:%M:%SZ')
+	out_file = open('wikiExtendedData.json', 'w')
 
+	while(stopToken):
+	    url = f'https://en.wikipedia.org/w/api.php?action=query&format=json&list=recentchanges&rcprop=ids%7Csizes%7Cfrcshow=!bot%7Clags%7Cuserid%7Ctimestamp&rclimit=500&rcstart={wikistamp}&rcdir=newer'
+	    print(url)
 
+	    data = call(url)
+	    content = data['query']['recentchanges']
+	    lastTime = content[-1]['timestamp']
+	    tCurrent = datetime.strptime(lastTime, "%Y-%m-%dT%H:%M:%SZ")
 
-while(stopToken):
-    url = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=recentchanges&rcprop=ids%7Csizes%7Cfrcshow=!bot%7Clags%7Cuserid%7Ctimestamp&rclimit=100&rcstart='+wikistamp+'&rcdir=newer'
-    print(url)
+	    content_str = ','.join(str(v) for v in content)
+	    content_str = content_str.replace('\'',r'"')
+	    out_file.write(content_str)
+	    timestamp = int(tCurrent.timestamp()) + 3601
+	    wikistamp = datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%dT%H:%M:%SZ')
 
-    data = call(url)
-    query = data['query']
-    content = query['recentchanges']
-    lastTime = content[-1]['timestamp']
-    lastTime = lastTime.replace('T',r' ')
-    lastTime = lastTime.replace('Z',r'')
-    tCurrent = datetime.strptime(lastTime, "%Y-%m-%d %H:%M:%S")
+	    if tCurrent >= tStop:
+	         break
 
-
-    contentStr = ','.join(str(v) for v in content)
-    contentStr = contentStr.replace('\'',r'"')
-    print(contentStr,end = "\r\n", file=open("wikiData.json", "a"))
-    timestamp = timestamp+900
-    wikistamp = datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%dT%H:%M:%SZ')
-
-    if tCurrent >= tStop:
-         break
+	out_file.close()
